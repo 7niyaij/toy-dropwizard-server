805e5dc1c44f659968e8bd1bfc6142855f94f19a
Jiayin <jiayin7@ualberta.ca>
2024-02-09 10:43:38 -0700

    Add workflow file

--- test_example.py
+++ test_example.py
@@ -2,11 +2,11 @@ import json
 import requests
 
 def test_code():
-    response=requests.get("http://localhost:8085")
-    json_response=response.json()
-    assert(json_response["code"]==404)
+    r = requests.get("http://localhost:8085")
+    json_response = r.json()
+    assert json_response["code"] == 404
 
 def test_message():
-    response=requests.get("http://localhost:8085")
-    json_response=response.json()
-    assert(json_response["message"]=="HTTP 404 Not Found")
+    r = requests.get("http://localhost:8085")
+    json_response = r.json()
+    assert json_response["message"] == "HTTP 404 Not Found"
